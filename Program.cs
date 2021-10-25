6f3b5977ccc8f7b38e21823571353470bd9cd1ed
CD Waddell <cdwaddell@gamil.com>
2021-10-25 19:53:25 -0400

    Fixed some bugs

--- Program.cs
+++ Program.cs
@@ -24,12 +24,18 @@ namespace SportApp
                         var matchingTeams = searchEngine.SearchTeams(searchTerm);
                         DisplayTeams(matchingTeams);
                         break;
+                    case 3:
+                        return;
                 }
             }
         }
 
         private static void DisplayTeams(List<Team> allTeams)
         {
+            if(allTeams.Count == 0)
+            {
+                Console.WriteLine("No teams found");
+            }
             var i = 0;
             foreach(var team in allTeams)
             {
@@ -39,8 +45,17 @@ namespace SportApp
 
         private static string GetTeamSearchTerm()
         {
-            Console.WriteLine("Enter a search term:");
-            return Console.ReadLine();
+            while(true)
+            {
+                Console.WriteLine("Enter a search term:");
+                var searchTerm = Console.ReadLine();
+                if(!string.IsNullOrWhiteSpace(searchTerm))
+                {
+                    return searchTerm;
+                }else{
+                    Console.WriteLine($"Invalid search term {searchTerm}");
+                }
+            }
         }
 
         private static int GetRootMenuOption()
@@ -63,6 +78,7 @@ namespace SportApp
             Console.WriteLine("What would you like to do?");
             Console.WriteLine("1) List All Teams");
             Console.WriteLine("2) Search Teams by Name");
+            Console.WriteLine("3) Exit");
         }
     }
 }
